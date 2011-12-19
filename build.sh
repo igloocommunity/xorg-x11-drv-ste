ca0c40b7ae49a045fd65aa0f62925968cdcd298e
Kalle Vahlman <kalle.vahlman@movial.com>
2011-12-19 06:45:04 +0000

    We are not always on a Snowball, can't use uname to search for headers
    Instead we look for our flavored headers in /usr/src, latest wins.

--- build.sh
+++ build.sh
@@ -4,7 +4,9 @@ export CROSS_COMPILE=${CROSS_COMPILE:=}
 
 make distclean
 
-export CPPFLAGS="-I/usr/src/linux-headers-`uname -r`/include -I/usr/include/libdrm"
+headers=$(ls -d -1 /usr/src/linux-headers-*-ux500 | tail -1)
+
+export CPPFLAGS="-I${headers}/include -I/usr/include/libdrm"
 ./configure --prefix=/usr
 
 make
